9762bad48f474206ffdd3ef43664e2994d46977f
rshah-nu <ronakshah2013@u.northwestern.edu>
2017-11-28 18:25:27 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Inquirer package used for User input
+var inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Guess a letter: ",
+            name: "userGuess"
+        }
+    ])
+    .then(function(inquirerResponse){
+        console.log(inquirerResponse.userGuess);
+    });
